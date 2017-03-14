57571e809bee7826920005aa6b96675c40426412
tomschreck <tom_schreck@solutiaconsulting.com>
2017-03-14 01:27:49 -0500

    switched to production URL

--- src/services/cdf-config.service.ts
+++ src/services/cdf-config.service.ts
@@ -1,5 +1,5 @@
 export class CdfConfigService 
 {
-	//static readonly CDF_WEBAPI_BASE_URL = 'https://webapi.cdf.cloud/api/';
-	static readonly CDF_WEBAPI_BASE_URL = 'http://cdf-api-local.webapi.cdf.cloud/api/';
+	static readonly CDF_WEBAPI_BASE_URL = 'https://webapi.cdf.cloud/api/';
+	//static readonly CDF_WEBAPI_BASE_URL = 'http://cdf-api-local.webapi.cdf.cloud/api/';
 }
